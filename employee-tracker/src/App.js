67fda52b1f6fd0080fcecf79e4a2ac9ecdce3d2f
Frank Merchlewitz <jlimhb@gmail.com>
2021-05-03 23:10:46 -0500

    staged previous commits in a smaller folder, so missed some updates

--- employee-tracker/src/App.js
+++ employee-tracker/src/App.js
@@ -6,7 +6,6 @@ import "./App.css";
 function App() {
     return (
         <div className="container">
-            <h1>Employee Tracker</h1>
             <Nav />
             <EmployeeDirectory />
         </div>
--- employee-tracker/src/components/Nav/index.js
+++ employee-tracker/src/components/Nav/index.js
@@ -2,25 +2,16 @@ import React from "react";
 import "./style.css";
 
 
-function Nav(props) {
+function Nav() {
     return (
-        // bootstrap navbar dark theme
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <header className="banner">
+            <h1>Employee Tracker</h1>
+            <p>
+            Welcome to your employee management dashboard. You can search employees by their name, and sort the order they appear by clicking on the button below. Happy managing!
+        </p>
+        </header>
 
-            <div className="navbar-collapse row" id="navbarNav">
-
-                {/* <div>
-                    <form className="form-inline">
-                        <input 
-                            value={props.search}
-                            onChange={props.handleInputChange}
-                            type="search"
-                            placeholder="Type Employee Last Name" 
-                            aria-label="Search" />
-                    </form>
-                </div> */}
-            </div>
-        </nav>
+        
 
 
     );
